dff223970d0d3b1461a60ec95aba22e2fd8d774e
AliFenton <tfenton@csumb.edu>
2018-02-11 14:13:32 -0800

    adding superherofile and more detail

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Ali Fenton on 2/7/18.
+//  Copyright © 2018 Ali Fenton. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
